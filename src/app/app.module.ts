64d8a0de3eef890a11f0714e109fe295e55adbe4
AsaP <alberto83980654@gmail.com>
2021-06-13 17:09:30 -0600

    master page [✔]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componet/navbar/navbar.component';
+import { HeaderComponent } from './componet/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    
   ],
   imports: [
     BrowserModule
