d556cee73dcb96a8740e43d90f1f1f1ff67fa961
AsaP <alberto83980654@gmail.com>
2021-06-13 19:25:42 -0600

    service Auth[✔]

--- src/app/Service/Auth.services.ts
+++ src/app/Service/Auth.services.ts
@@ -1,8 +1,59 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
+import { Credenciales } from '../interface/loging';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    
+    usuario: any = {};
+    constructor(private firestore: AngularFirestore,
+        public auth: AngularFireAuth) {
+
+        this.auth.authState.subscribe(user => {
+
+            console.log('Estado Usuario', user);
+
+            if (!user) {
+                return;
+            }
+            this.usuario.nombre = user.displayName;
+            this.usuario.uid = user.uid;
+
+        })
+
+    }
+
+    //  auth
+    login(creden: Credenciales) {
+
+        firebase.auth().signInWithEmailAndPassword(creden.email, creden.password)
+            .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+            });
+    }
+
+    register(creden: Credenciales) {
+
+        firebase.auth().createUserWithEmailAndPassword(creden.email, creden.password)
+            .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+            });
+    }
+
+
 }
